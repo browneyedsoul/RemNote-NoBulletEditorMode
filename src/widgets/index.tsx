59fb83bce1a5d026eefd9ba3d80ec22492ba755c
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-06 15:27:58 +0900

    Update command

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,30 @@
 import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-async function onActivate(plugin: ReactRNPlugin) {}
+export const [BULLET_LIST, BULLET_LISTS] = "bulletlist, bulletlists";
+
+async function onActivate(plugin: ReactRNPlugin) {
+  await plugin.app.registerPowerup("Bulletlist", BULLET_LIST, "A Power-up Block for making a bullet", { slots: [] });
+  await plugin.app.registerPowerup("Bulletlists", BULLET_LISTS, "A Power-up Block for making bullets", { slots: [] });
+
+  await plugin.app.registerCommand({
+    id: "bulletlist",
+    name: "Bulletlist",
+    description: "Make a bullet",
+    action: async () => {
+      const rem = await plugin.focus.getFocusedRem();
+      await rem?.addPowerup(BULLET_LIST);
+    },
+  });
+  await plugin.app.registerCommand({
+    id: "bulletlists",
+    name: "Bulletlists",
+    description: "Make bullets",
+    action: async () => {
+      const rem = await plugin.focus.getFocusedRem();
+      await rem?.addPowerup(BULLET_LISTS);
+    },
+  });
+}
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
