f35ce8f8f6fe1008f23694b3db4fe6a909daf871
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-06 15:37:42 +0900

    Fix destucturing syntax

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,10 +1,10 @@
 import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-export const [BULLET_LIST, BULLET_LISTS] = "bulletlist, bulletlists";
+export const [BULLET_LIST, BULLET_LISTS] = ["bulletlist", "bulletlists"];
 
 async function onActivate(plugin: ReactRNPlugin) {
-  await plugin.app.registerPowerup("Bulletlist", BULLET_LIST, "A Power-up Block for making a bullet", { slots: [] });
-  await plugin.app.registerPowerup("Bulletlists", BULLET_LISTS, "A Power-up Block for making bullets", { slots: [] });
+  await plugin.app.registerPowerup("bulletlist", BULLET_LIST, "A Power-up Block for making a bullet", { slots: [] });
+  await plugin.app.registerPowerup("bulletlists", BULLET_LISTS, "A Power-up Block for making bullets", { slots: [] });
 
   await plugin.app.registerCommand({
     id: "bulletlist",
