182c35443cd9c37e3ed6583c7b914cd9ac674353
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-13 12:44:49 +0900

    Fix mobile rendering issue

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -2,7 +2,18 @@ import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
 export const [BULLET_LIST, BULLET_LISTS] = ["bulletlist_power-up", "bulletlists_power-up"];
 
+let NoBulletCSS: string;
+
 async function onActivate(plugin: ReactRNPlugin) {
+  await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-NoBulletEditorMode/main/src/snippet.css")
+    .then((response) => response.text())
+    .then((text) => {
+      NoBulletCSS = text;
+      console.log("No Bullet!!");
+    })
+    .catch((error) => console.error(error));
+  await plugin.app.registerCSS("No Bullet Anymore!", NoBulletCSS);
+
   await plugin.app.registerPowerup("Bulletlist", BULLET_LIST, "A Power-up Block for making a bullet", { slots: [] });
   await plugin.app.registerPowerup("Bulletlists", BULLET_LISTS, "A Power-up Block for making bullets", { slots: [] });
 
